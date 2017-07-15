f69dc7eb8c91091ca4eb34d27f3c1873ca9cbad6
stellasoft-george <george@stellasoft.co.uk>
2017-07-15 16:42:01 +0100

    add error handling

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "hello world";
+namespace App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+$whoops = new \Whoops\Run;
+
+if ($environment == 'development') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e) {
+        echo 'Todo: Friendly error handling';
+    });
+}
+
+$whoops->register();
+
+throw new \Exception;
