6bc940a76efecf5885e9b8a01bfdd26f67eee08f
stellasoft-george <george@stellasoft.co.uk>
2017-07-15 17:14:40 +0100

    remove route handlers to controller class

--- src/Routes.php
+++ src/Routes.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-    ['GET', '/hello-world', function () {
-        echo 'Hello World';
-    }],
-    ['GET', '/another-route', function () {
-        echo 'This works too';
-    }],
+    ['GET', '/', ['App\Controllers\Homepage', 'show']],
+//    ['GET', '/another-route', function () {
+//        echo 'This works too';
+//    }],
 ];
